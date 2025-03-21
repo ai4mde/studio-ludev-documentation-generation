e61ee1719d5261e5c71a5434452f59fc97f493d2
Scooter1337 <lucav007@hotmail.com>
2025-03-21 14:03:30 +0100

    added edit button

--- frontend/src/lib/features/prototypes/components/DocsButton.tsx
+++ frontend/src/lib/features/prototypes/components/DocsButton.tsx
@@ -1,5 +1,5 @@
 import { Modal, ModalDialog } from '@mui/joy';
-import { BookText, Check, Copy, Loader2, Save, X } from "lucide-react";
+import { BookText, Check, Copy, Loader2, Pencil, Save, X } from "lucide-react";
 import { useState } from "react";
 
 export function DocsButton() {
@@ -7,6 +7,7 @@ export function DocsButton() {
     const [docs, setDocs] = useState("");
     const [docsLoading, setDocsLoading] = useState(false);
     const [copied, setCopied] = useState(false);
+    const [editing, setEditing] = useState(false);
 
     const showDocs = async () => {
 
@@ -77,11 +78,20 @@ This document provides an overview and detailed description of the UML prototype
 
                         <div className="flex gap-2">
 
+
                         <button
-                            onClick={copyDocs}
+                                onClick={() => {
+                                    setEditing(!editing);
+                                    console.log("Editing: ", editing);
+
+                                }}
                             className="w-[40px] h-[40px] bg-gray-500 text-white rounded-md hover:bg-gray-600 flex items-center justify-center shrink-0"
-                            >
-                            {copied ? <Check className="size-5" /> : <Copy className="size-5" />}
+                        >
+                                {!editing ? (<Pencil className="size-5" />) : (
+
+<svg xmlns="http://www.w3.org/2000/svg" className="size-5" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-off"><path d="m10 10-6.157 6.162a2 2 0 0 0-.5.833l-1.322 4.36a.5.5 0 0 0 .622.624l4.358-1.323a2 2 0 0 0 .83-.5L14 13.982"/><path d="m12.829 7.172 4.359-4.346a1 1 0 1 1 3.986 3.986l-4.353 4.353"/><path d="m15 5 4 4"/><path d="m2 2 20 20"/></svg>
+
+                            )}
                             </button>
 
 
@@ -90,13 +100,20 @@ This document provides an overview and detailed description of the UML prototype
                             className="w-[40px] h-[40px] bg-gray-500 text-white rounded-md hover:bg-gray-600 flex items-center justify-center shrink-0"
                         >
                             <Save className="size-5" />
-                        </button>
+                            </button>
+
+                            <button
+                            onClick={copyDocs}
+                            className="w-[40px] h-[40px] bg-gray-500 text-white rounded-md hover:bg-gray-600 flex items-center justify-center shrink-0"
+                            >
+                            {copied ? <Check className="size-5" /> : <Copy className="size-5" />}
+                            </button>
 
                             </div>
                     </div>
 
                     <div className="flex h-full w-full flex-col gap-1 p-3">
-                        <pre contenteditable>{docs}</pre>
+                        <pre contentEditable={editing}>{docs}</pre>
                     </div>
                 </ModalDialog>
             </Modal>
